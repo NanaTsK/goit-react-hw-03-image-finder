1e40ffdd42b48fe3daa536bddcfec8e6afef9c25
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-17 23:17:56 +0200

    Load more

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,25 +1,23 @@
 import styled from 'styled-components';
 
-export const LoadMoreBtn = styled.button`
-  padding: 8px 16px;
-  border-radius: 2px;
-  background-color: #3f51b5;
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  text-align: center;
-  display: inline-block;
-  color: #fff;
-  border: 0;
-  text-decoration: none;
-  cursor: pointer;
-  font-family: inherit;
-  font-size: 18px;
-  line-height: 24px;
-  font-style: normal;
-  font-weight: 500;
-  min-width: 180px;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-  &:hover {
-    background-color: #303f9f;
-  }
-`;
+export const LoadMoreBtn = styled('button')({
+  padding: '12px 16px',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  borderRadius: '2px',
+  backgroundColor: 'rgba(38, 192, 211, 0.6)',
+  transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  border: '0',
+  textAlign: 'center',
+  textDecoration: 'none',
+  width: '100vw',
+
+  color: '#fff',
+  boxShadow:
+    '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
+
+  '&:is(:hover, :focus)': {
+    backgroundColor: 'rgba(38, 192, 211, 0.8)',
+  },
+});
