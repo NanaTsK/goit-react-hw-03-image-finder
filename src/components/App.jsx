128d8080d7b55ac832e8a9e4431cc940ce603444
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-17 16:45:53 +0200

    fetchInputImages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,97 @@
 import { Component } from 'react';
 import { Container } from './index.styled';
 import { Searchbar } from './Searchbar/Searchbar';
-/* <Searchbar>, <ImageGallery>, <ImageGalleryItem>, <Loader>, <Button>, <Modal></Modal> */
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { fetchImages } from '../api/pixabay-api';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const notifyInit = Notify.init({
+  width: '280px',
+  position: 'center-center',
+  distance: '20px',
+  opacity: 0.8,
+  fontSize: '20px',
+  borderRadius: '50px 10px',
+  notiflixIconColor: 'rgba(0,0,0,0.6)',
+  pauseOnHover: true,
+});
+
+// Notify.success(`xxx`, notifyInit);
+
+/* <Loader/>, <Button/>, <Modal/> */
 
 export class App extends Component {
-  state = {};
+  state = {
+    images: [],
+    error: false,
+    loader: false,
+    searchQuery: '',
+    // isShowModal: false,
+    // imageForModal: '',
+    page: 1,
+    // totalHits: null,
+    // result: null,
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.searchQuery !== this.state.searchQuery ||
+      prevState.page !== this.state.page
+    )
+      this.fetchInputImages();
+  }
+
+  fetchInputImages = async () => {
+    try {
+      this.setState({ loader: true });
+      const { data } = await fetchImages(
+        this.state.searchQuery,
+        this.state.page
+      );
+      if (!data.hits.length) {
+        Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`,
+          notifyInit
+        );
+        return;
+      }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...data.hits],
+        // result: this.state.page * 12,
+        // totalHits: data.totalHits,
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loader: false });
+    }
+  };
+
+  handleSubmit = searchQuery => {
+    this.setState({ searchQuery, page: 1, images: [] }, this.fetchInputImages);
+  };
+
   render() {
+    const {
+      images,
+      // loader,
+      error,
+      // isShowModal,
+      // imageForModal,
+      // totalHits,
+      // result,
+    } = this.state;
+    const { getModalPhoto } = this;
+    // const { handleSubmit, getModalPhoto, getMorePhoto, closeModal } = this;
+
     return (
       <Container>
-        <Searchbar></Searchbar>
+        <Searchbar onSubmit={this.handleSubmit} />
+        {images && images.length > 0 && (
+          <ImageGallery images={images} getModalPhoto={getModalPhoto} />
+        )}
+        {error && <p>Oooops! Something went wrong...</p>}
       </Container>
     );
   }
