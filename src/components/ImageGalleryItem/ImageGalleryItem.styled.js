652fa32fea3b0f45975528c41ed239c198b682aa
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-17 19:20:52 +0200

    ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components';
 
+const transition = '250ms cubic-bezier(0.4, 0, 0.2, 1)';
+
 export const GalleryItem = styled('li')({
-  borderRadius: '2px',
+  borderRadius: '4px',
   boxShadow:
     '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)',
-
-  '&:hover': {
-    transform: 'scale(1.03)',
-    cursor: 'zoom-in',
-  },
-  transition: 'transform 250ms cubic-bezier(0.4, 0, 0.2, 1)',
 });
 export const GalleryImg = styled('img')({
+  borderRadius: '4px',
+
   width: '100%',
   height: '260px',
   objectFit: 'cover',
@@ -20,23 +18,5 @@ export const GalleryImg = styled('img')({
     transform: 'scale(1.03)',
     cursor: 'zoom-in',
   },
-  transition: 'transform 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  transition: `transform ${transition}, cursor ${transition}`,
 });
-
-// .ImageGalleryItem {
-//   border-radius: 2px;
-//   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-//     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-// }
-
-// .ImageGalleryItem-image {
-//   width: 100%;
-//   height: 260px;
-//   object-fit: cover;
-//   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
-// }
-
-// .ImageGalleryItem-image:hover {
-//   transform: scale(1.03);
-//   cursor: zoom-in;
-// }
