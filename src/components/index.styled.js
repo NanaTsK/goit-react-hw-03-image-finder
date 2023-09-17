28ee0fcf2327dc762222a2e21a6d1f99dea6f4e7
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-17 19:22:17 +0200

    index styled

--- src/components/index.styled.js
+++ src/components/index.styled.js
@@ -1,8 +1,12 @@
 import { styled } from 'styled-components';
 
-export const Container = styled.div`
-    display: 'grid',
-    gridTemplateColumns: '1fr',
-    gridGap: '16px',
-    paddingBottom: '24px',
-`;
+export const Container = styled('div')({
+  //   display: 'grid',
+  //   gridTemplateColumns: 'repeat(auto-fill, minmax(320px, 1fr))',
+  //   //   gridTemplateColumns: '1fr',
+  //   gridGap: '16px',
+  //   marginTop: '0',
+  //   margin: '20px auto ',
+  //   padding: '0 16px',
+  //   maxWidth: '1366px',
+});
